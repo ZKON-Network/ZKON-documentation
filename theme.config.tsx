82781ba819d06d3d14ce340f255959d757672a20
Juanjo D. <juanjosediazcozar@gmail.com>
2024-07-25 16:31:19 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>ZKON Network</span>,
   project: {
-    link: 'http://zkon.xyz/',
+    link: 'https://github.com/ZKON-Network',
   },
   chat: {
     link: 'https://discord.gg/AnmcW4HY2M',
   },
   docsRepositoryBase: 'https://github.com/ZKON-Network',
   footer: {
-    text: 'ZKON Github',
+    text: 'ZKON Network 2024',
+    link: 'https://zkon.xyz',
   },
 }
 
