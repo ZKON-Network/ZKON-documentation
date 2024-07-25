f087ed261f36f13b054f1b9100f4d8490461754b
Juanjo D. <juanjosediazcozar@gmail.com>
2024-07-25 17:12:47 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,5 @@ const withNextra = require('nextra')({
 })
 
 module.exports = withNextra({
-  async rewrites() {
-    return [
-      {
-        source: '/documentation/:path*',
-        destination: '/:path*', // could be '/docs/:path*' if your documentation is in a 'docs' directory
-      },
-    ];
-  },
-});
+  basePath: '/documentation',
+})
