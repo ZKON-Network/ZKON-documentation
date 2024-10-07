f2f2a25d4d08150805a907c34437be32157fdea0
Juanjo Díaz <juanjosediazcozar@gmail.com>
2024-10-07 17:04:49 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import Head from 'next/head';
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>ZKON Network</span>,
@@ -12,14 +13,6 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'ZKON Network 2024',
   },
-  head: () => (
-    <Head>
-      <meta property="og:title" content="ZKON Network Documentation" />
-      <meta property="og:description" content="Learn about ZKON Network" />
-      <meta property="og:image" content="/path-to-your-image.png" />
-      <meta property="og:url" content="https://zkon.network/docs" />
-    </Head>
-  ),
-};
+}
 
-export default config;
+export default config
